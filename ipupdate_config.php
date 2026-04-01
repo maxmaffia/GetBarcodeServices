9908fad6f6ee1bc156f95f6e52a7bf847d18ecfb
Massimiliano Maffia <m.maffia.external@bizeta.net>
2026-04-01 18:19:43 +0200

    chore: update ipupdate configuration

--- ipupdate_config.php
+++ ipupdate_config.php
@@ -1,5 +1,5 @@
 <?php
 // Configurazione servizio ipupdate
 define('IPUPDATE_ID_AZIENDA', 1);                 // <-- imposta l'ID azienda
-define('IPUPDATE_AZIENDA', 'Nome Azienda Srl');   // <-- imposta il nome azienda
+define('IPUPDATE_AZIENDA', 'Test');   // <-- imposta il nome azienda
 define('IPUPDATE_API_KEY', 'UElX7xtRmJW1VNnm8kOJpG6uNzvv7vSOG8_j5iUF5CzZhPRMOUZqqMsWi3P0L1LbGg4ps7a2QWU4vEY9QJGYq7SM2UKNLhRgogIrQ5ypL0hh3Uxnil9AuSxotcSy6ydw'); // <-- chiave condivisa con /services/config.php
